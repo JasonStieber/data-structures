74e4043b690448fa0392e84a18404ea056bea70e
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:26:50 -0700

    completed functional queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,7 @@
 var Queue = function() {
   var someInstance = {};
+  var nowServing = 0;
+  var lastInLine = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +9,26 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    lastInLine++;
+    storage[lastInLine] = value;
   };
 
   someInstance.dequeue = function() {
+    if (lastInLine <= nowServing) {
+      return 0;
+    } else {
+      nowServing++;
+      return storage[nowServing];
+    }
+  
   };
 
   someInstance.size = function() {
+    if (lastInLine <= nowServing) {
+      return 0;
+    }
+    return lastInLine - nowServing;
+    
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,29 @@
 var Stack = function() {
   var someInstance = {};
-
+  let size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value; 
+    size++;
+    // console.log(size - 1 + ' is the locatoin of ' + storage[size - 1 ]);
   };
 
   someInstance.pop = function() {
+    if (size === 0) {
+      return 0;
+    } else {
+      size--;
+      var theGoods = storage[size];
+      console.log(theGoods);
+      return theGoods;
+    }  
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
