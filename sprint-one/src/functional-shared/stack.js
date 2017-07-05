7730a284a0b0fc7da27711349d8fe6248c8bf2d8
Hack Reactor Student <student@hackreactor.com>
2017-07-05 16:41:00 -0700

    started shared methods function

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,20 @@
 var Stack = function() {
+  let size = 0;
+  let storage = {};
+  
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 };
 
-var stackMethods = {};
+var stackMethods = { 
+  this.push: function(value){
+    storage[this.size] = value;
+    this.size++;
+  },
+  this.pop:  function(){},
+  this.size: function(){
+    return this.size;
+  }
+};
 
 
