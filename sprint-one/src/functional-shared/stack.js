6a80e775e3e4c4685d54700c855ab5ef9aa692a5
Hack Reactor Student <student@hackreactor.com>
2017-07-05 20:31:02 -0700

    nananannanan BATMAN

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,11 +1,44 @@
+// var Stack = function() {
+//   let constructor = {};
+//   constructor.size = 0;
+//   constructor.storage = {};
+//   _.extend(constructor, stackMethods);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+//   // constructor.push = stackMethods.push;
+//   // constructor.pop = stackMethods.pop;
+//   constructor.size = stackMethods.size.bind(this);
+//   return constructor;
+  
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+// };
+
+// var stackMethods = {};
+
+// //stackMethods.size = 0;
+
+// //stackMethods.storage = {};
+
+// stackMethods.push = function(value) {
+//   this.storage[this.size] = value;
+//   this.size++;
+// };
+
+// stackMethods.pop = function() {
+//   this.size--;
+//   return this.storage[this.size]; 
+// };
+
+// stackMethods.size = function() {
+//   return this.size;
+// };
 var Stack = function() {
   let constructor = {};
-  let size = 0;
-  let storage = {};
-  // _.extend(constructor, stackMethods);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-  constructor.push = stackMethods.push;
-  constructor.pop = stackMethods.pop;
-  constructor.size = stackMethods.size;
+  constructor.pos = 0;
+  constructor.storage = {};
+  _.extend(constructor, stackMethods);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+  // constructor.push = stackMethods.push;
+  // constructor.pop = stackMethods.pop;
+  //  constructor.size = stackMethods.size.bind(this);
   return constructor;
   
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
@@ -14,19 +47,27 @@ var Stack = function() {
 
 var stackMethods = {};
 
+//stackMethods.size = 0;
+
+//stackMethods.storage = {};
+
 stackMethods.push = function(value) {
-  this.storage[this.size] = value;
-  this.size++;
+  this.pos++;
+  this.storage[this.pos] = value;
+  
 };
 
 stackMethods.pop = function() {
-  this.size--;
-  return this.storage[this.size]; 
+  this.pos--;
+  return this.storage[this.pos]; 
 };
 
 stackMethods.size = function() {
-  return this.size;
+  return this.pos;
 };
 
-
-
+var baller  = Stack();
+baller.push("batman");
+console.log(baller);
+console.log(baller.size);
+console.log(baller.pop());
