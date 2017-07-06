1979da9a63eb3b90f0945203ec848647f920f97e
Hack Reactor Student <student@hackreactor.com>
2017-07-06 16:14:20 -0700

    murdered the hell out that function "

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (this.tail === null) {
+      this.tail = Node(value);
+      this.head = this.tail;
+    } else {
+      var tempNode = Node(value);
+      this.tail.next = tempNode;
+      this.tail = tempNode;
+    }
   };
 
   list.removeHead = function() {
+    var tempNode = this.head;    
+    if (this.head.next !== null) {
+      this.head = tempNode.next; 
+      return tempNode.value;
+    } else if (this.head.next === null) {
+      this.head = null;
+      this.tail = null;
+      return tempNode.value;  
+    }
   };
 
   list.contains = function(target) {
+    var element = this.head;
+    while (element !== null) {
+      if (element.value === target) {
+        return true;
+      } else {
+        element = element.next;
+      } 
+    } return false;
   };
 
   return list;
