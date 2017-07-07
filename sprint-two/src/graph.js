42588f58f1c4933a94aa1165c0c9333e10669cf7
Hack Reactor Student <student@hackreactor.com>
2017-07-06 22:39:57 -0700

    completed 4 of the 6 functions for graph "

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,34 +2,54 @@
 
 // Instantiate a new graph
 var Graph = function() {
+//   this.node = {value: null, edges: []};
+  this.nodes = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes[node] = [];  
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for (var i = 0; i < this.nodes[node].length; i++) {
+    var connection = this.nodes[node][i];
+    this.removeEdge(node, connection);
+  }
+  delete this.nodes[node];
+  
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.nodes[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.nodes[fromNode].push(toNode);
+  this.nodes[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  var index = this.nodes[fromNode].indexOf(toNode);
+  this.nodes[fromNode].splice(index, 1);
+  
+  index = this.nodes[toNode].indexOf(fromNode);
+  this.nodes[toNode].splice(index, 1);
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -16,36 +16,16 @@ treeMethods.addChild = function(value) {
 };
 
 treeMethods.contains = function(target) {
-  // var disTree = this;
-  // var exists = false;
-  // if (disTree.value === target) {
-  //    return true;
-  // }
-  // for( var i = disTree.children.length; i++) {
-  //   if (disTree.children[i].value === target){
-  //     return exits || disTree[];
-  //   }    
-    
-  // }
-   
-  // else {
-  //   var exist = false;
-  //   for (var i = 0; i < this.children.length; i++) {
-  //     var bbT = this.children[i];//stands for BabyTree
-  //     exist = bbT.contains(target);  
-  //   }
-  // }
-  var theForest = [this].concat(this.children);
-  for (var i = 1; i < theForest.length; i++) {
+  var theForest = [this];
+  for (var i = 0; i < theForest.length; i++) {
     if (theForest[i].children !== null) {
       theForest = theForest.concat(theForest[i].children);
-    } console.log('how many trees in the forest')
+    }
   } for (var j = 0; j < theForest.length; j++) {
     if (theForest[j].value === target) {
       return true;
     }
-  } return false;
-      
+  } return false; 
 }; 
 
 /*
