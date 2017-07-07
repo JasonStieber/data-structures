e4ef50c01f96ee174eddfbc8284490966065dbe1
Hack Reactor Student <student@hackreactor.com>
2017-07-07 14:36:50 -0700

    three lines to complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,30 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  //input: item
+  //result: {item} add it to the data set
+  //complexities:  adding an existing value should not increase the set size
+  //pass in value as key, value in _storage obj
+
+  this._storage[item] = item;
+  
 };
 
 setPrototype.contains = function(item) {
+
+  return (this._storage[item] === item);
+
 };
 
 setPrototype.remove = function(item) {
+
+  delete this._storage[item];
 };
 
 /*
