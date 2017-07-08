77cc0ef355d06f0afbfee423c38ce49bb4255a8f
Hack Reactor Student <student@hackreactor.com>
2017-07-07 20:39:41 -0700

    made progress on hash tables still have a bug

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,17 +3,11 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  // this._storage.each(function(item, index, collection){
-  //   this.set(index, []);
-  // });
   this.elements = 0;
- };
+  console.log(getIndexBelowMaxForKey('Allan',16));
+  console.log(getIndexBelowMaxForKey('Allan',8))
+};
 
-  // limitedArray.each = function(callback) {
-  //   for (var i = 0; i < storage.length; i++) {
-  //     callback(storage[i], i, storage);
-  //   }
-  // };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
@@ -35,15 +29,20 @@ HashTable.prototype.insert = function(k, v) {
   
 };
 
+HashTable.prototype.showMe = function() {
+  this._storage.each(function(val) { if (val !== undefined) { console.log(val[0]); } } );
+};
+
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var tuples = this._storage.get(index);
-  for (var i = 0; i < tuples.length; i++) {
-    if (tuples[i][0] === k) {
-      return tuples[i][1];
-    }
-  } return undefined;
-  throw 'no key here check round back batman ';
+  if (tuples !== undefined) {
+    for (var i = 0; i < tuples.length; i++) {
+      if (tuples[i][0] === k) {
+        return tuples[i][1];
+      }
+    } return undefined;
+  } // throw 'no key here check round back batman ';
 };
 
 HashTable.prototype.remove = function(k) {
@@ -59,11 +58,37 @@ HashTable.prototype.remove = function(k) {
 };
 
 HashTable.prototype.resize = function () {
-  if (this.elements / this._limit > 0.75) {  //size up
-  }
-  else if (this.elements / this._limit < 0.25) {      // size down
-  }
-  else {
+  var tempStorage = this._storage;
+ // this.showMe(); 
+  if (this.elements / this._limit > 0.75) {
+    this._limit *= 2;
+    this._storage = LimitedArray(this._limit);
+    tempStorage.each(function(cell, index, collection) {
+      if (cell !== undefined) {
+        for (var i = 0; i < cell.length; i++) {
+          if (cell[i] !== undefined) {
+            this.insert(cell[i][0], cell[i][1]);
+          }
+        }
+      }  
+    }.bind(this)); 
+    throw "did we get here?";
+    console.log(this._storage.get(8)[0]);
+  } else if (this.elements / this._limit < 0.25) {      // size down
+    if (this._limit !== 8) { 
+      this._limit /= 2;
+      this._storage = LimitedArray(this._limit);
+      tempStorage.each(function(cell, index, collection) {
+        if (cell !== undefined) {
+          for (var i = 0; i < cell.length; i++) {
+            if (cell[i] !== undefined) {
+              this.insert(cell[i][0], cell[i][1]);
+            }
+          }
+        }  
+      }.bind(this));
+    }
+  } else {
     // throw 'You done goofed son! why no up or down??? see call to resize funtion./..... batman'; 
   }
 };
